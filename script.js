eeffe72c1b5d5712eccb6524126525250aa4c396
c20245375paclibar <c20245375.paclibar@csav.edu.ph>
2026-09-04 14:37:00 +0800

    variables (let and const)

--- script.js
+++ script.js
@@ -1,2 +1,14 @@
-console.log("Hello, World!");
+console.log("Hello, World.");
 console.log("From script.js file.");
+
+const myName = "Elsie";
+let age = 60;
+age = 16;
+number = 12345678;
+const address = "Hda. Angelina II, Barangay XII";
+
+console.log(`Name: ${myName}`);
+console.log(`Age: ${age}`);
+console.log(`Number: ${number}`);
+console.log(`Address: ${address}`);
+console.log(67);
